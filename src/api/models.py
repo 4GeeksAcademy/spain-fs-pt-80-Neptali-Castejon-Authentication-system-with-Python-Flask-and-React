bdeec58f5f891051fa8fe1d1b1fad60e336127ff
Neptali Castejón <63590772+Neptali-Ricardo@users.noreply.github.com>
2024-12-19 13:04:00 +0000

    Initial setup: updated Pipfile, Pipfile.lock, and Flask API structure. Added migrations folder.

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Users {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            "address": self.address.serialize() if self.address else None
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,11 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Users
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,9 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/test', methods=['GET'])
+def test():
+    return jsonify({"msg": "OK"}), 200
+
